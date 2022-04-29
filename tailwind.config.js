ca24c8d3966badb94a78282b2158c373d2c8e7ef
oliver-gomes <ogomesphoto@gmail.com>
2022-04-29 10:27:07 -0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontSize: {
+        sm: ["14px"],
+      },
+      colors: {
+        starGreen: "#D4E9E2",
+        darkGreen: "#1E3932",
+        lightText: "rgba(0,0,0, .58)",
+        titleText: "rgba(0,0,0, .87)",
+      },
+    },
+  },
+  plugins: [],
+};
